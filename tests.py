aee7f9a97221d9d954d7691a8ee53e5a1d6c0c2a
Jose <jcuello@gmail.com>
2025-10-28 13:30:04 -0400

    Adds more unit tests to get_files_info

--- tests.py
+++ tests.py
@@ -3,16 +3,32 @@ from functions.get_files_info import get_files_info
 
 class TestFunctions(unittest.TestCase):
   def test_calculator_curent_dir(self):
-    expected_files = ["pkg", "main.py", "tests.py"]
-    test_results = {}
-    print("Result from current directory:")
+    print("\nResult from current directory:")
     lines = self.get_files_info_lines("calculator", ".")
     for l in lines:
       print(l)
 
+  def test_pkg_dir(self):
+    print("\nResult for 'pkg' directory:")
+    lines = self.get_files_info_lines("calculator", "pkg")
+    for l in lines:
+      print(l)
+
+  def test_bin_dir(self):
+    print("\nResult for '/bin' directory:")
+    lines = self.get_files_info_lines("calculator", "/bin")
+    for l in lines:
+      print(l) 
+
+  def test_previous_dir(self):
+    print("\nResult for '../' directory:")
+    lines = self.get_files_info_lines("calculator", "../")
+    for l in lines:
+      print(l) 
+
   def get_files_info_lines(self, working_directory, directory="."):
-    return list(get_files_info(working_directory, directory)
-                .split("\n"))
+    lines = get_files_info(working_directory, directory).split("\n")
+    return list(filter(lambda x: not x.startswith("- __pycache__"), lines))
 
 if __name__ == "__main__":
   unittest.main()
