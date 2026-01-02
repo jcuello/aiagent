49945150475cbe74e99b5b2344e42c11ffab95c9
Jose J Cuello <jcuello@gmail.com>
2026-01-02 18:58:43 -0500

    Adds agent loop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import sys
-from typing import Any
 import config
 from dotenv import load_dotenv
 
@@ -35,46 +34,56 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
 
-    response = client.models.generate_content(
-        model='gemini-2.5-flash',
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[
-                functions.get_files_info.available_functions,
-                functions.get_file_content.available_functions,
-                functions.write_file.available_functions,
-                functions.run_python_file.available_functions,
-            ],
-            system_instruction=config.SYSTEM_PROMPT,
+    function_results: list[types.Part] = []
+    for _ in range(20):
+        response = client.models.generate_content(
+            model='gemini-2.5-flash',
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[
+                    functions.get_files_info.available_functions,
+                    functions.get_file_content.available_functions,
+                    functions.write_file.available_functions,
+                    functions.run_python_file.available_functions,
+                ],
+                system_instruction=config.SYSTEM_PROMPT,
+            )
         )
-    )
-    
-    if "--verbose" in sys.argv:
-        verbose = True     
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    function_responses: list[types.Part] = []
-    if response.function_calls != None:
-        function_call = response.function_calls[0]
-        function_call_result = call_function(function_call, verbose)
+        if response.candidates:
+            messages.extend([c.content for c in response.candidates if c.content != None])
+        
+        if "--verbose" in sys.argv:
+            verbose = True     
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-        if function_call_result.parts == None or len(function_call_result.parts) == 0:
-            raise Exception("function_call_result.parts is empty.")
+        if response.function_calls != None:
+            function_call = response.function_calls[0]
+            function_call_result = call_function(function_call, verbose)
+
+            if function_call_result.parts == None or len(function_call_result.parts) == 0:
+                raise Exception("function_call_result.parts is empty.")
+            
+            func_response = function_call_result.parts[0].function_response
+            if func_response == None:
+                raise Exception("parts[0].function_response is empty.")
+            
+            function_results.append(function_call_result.parts[0])
+            if verbose:
+                print(f"-> {func_response.response}")
+            
+            messages.append(types.Content(role="user", parts=function_results))
         
-        func_response = function_call_result.parts[0].function_response
-        if func_response == None:
-            raise Exception("parts[0].function_response is empty.")
-        
-        function_responses.append(function_call_result.parts[0])
-        if verbose:
-            print(f"-> {func_response.response}")
-        
-    else:
-        # if no function was called, Gemini will give a text reply such as an
-        # error or confirmation e.g. "I can show you the files in the root directory. Would you like me to do that?"
-        print(response.text)
+        else:
+            # if no function was called, Gemini will give a text reply such as an
+            # error or confirmation e.g. "I can show you the files in the root directory. Would you like me to do that?"
+            print(response.text)
+            sys.exit(0)            
+               
+    print("Agent could not produce a solution.")
+    sys.exit(1)
 
 def call_function(function_call:types.FunctionCall, verbose=False):
     function_name = function_call.name or ""
